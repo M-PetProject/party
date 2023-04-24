d181c0d0c0bd937451a079652044115d41c81978
benny <benny@mkcreative.co.kr>
2023-04-24 15:56:58 +0900

    Vo 설명 추가

--- src/main/java/com/study/party/jpa/entity/cm_food/CmFoodEntity.java
+++ src/main/java/com/study/party/jpa/entity/cm_food/CmFoodEntity.java
@@ -2,6 +2,7 @@ package com.study.party.jpa.entity.cm_food;
 
 
 import com.study.party.jpa.entity.comm.DefaultEntity;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,12 +16,17 @@ import javax.persistence.*;
 @AllArgsConstructor
 @Table(name = "cm_food")
 @Entity
+@Schema(description="사용자가 등록한 음식 정보")
 public class CmFoodEntity extends DefaultEntity {
 
     private static final long serialVersionUID = 145828767031252846L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description="사용자가 등록한 음식 정보 IDX")
     private long foodIdx;
+
+    @Schema(description="사용자가 등록한 음식 이름")
     private String foodNm;
+
 }
