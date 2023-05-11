23ad7f5787009ef83b05a2ecec636791dff9204b
benny <benny@mkcreative.co.kr>
2023-05-11 10:39:41 +0900

    swagger 설정 수정

--- src/main/java/com/study/party/config/security/SecurityConfig.java
+++ src/main/java/com/study/party/config/security/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                    // 로그인, 회원가입 API 는 토큰이 없는 상태에서 요청이 들어오기 때문에 permitAll 설정
                    .and()
                    .authorizeRequests()
-                   .antMatchers("/","/comm/**", "/sample/**", "/v/**","/resources/**","/auth/**","/swagger/**","/swagger-resources/**","/webjars/**","/v2/api-docs","/v3/api-docs","/swagger-ui/**", "/api/v2/**", "/api/v3/**","/health" ).permitAll()
+                   .antMatchers("/","/comm/**", "/sample/**", "/v/**","/resources/**","/auth/**","/swagger/**","/swagger-resources/**","/webjars/**","/v2/api-docs","/v3/api-docs/**","/swagger-ui/**", "/api/v2/**", "/api/v3/**","/health" ).permitAll()
                    .anyRequest().authenticated()   // 나머지 API 는 전부 인증 필요
                    // JwtFilter 를 addFilterBefore 로 등록했던 JwtSecurityConfig 클래스를 적용
                    .and()
